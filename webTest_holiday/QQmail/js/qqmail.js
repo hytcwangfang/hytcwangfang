4638bb7bd5fe32e92c7f7478b77be6d0d2251180
hytcwangfang <2445923320@qq.com>
2014-08-21 11:36:03 +0800

    2014.8.21.1
    QQ邮箱首页静态完成
    tripNet静态页开始布局

--- webTest_holiday/QQmail/js/qqmail.js
+++ webTest_holiday/QQmail/js/qqmail.js
@@ -4,4 +4,12 @@ $(function(){
 		$(".contentright").hide();
 		$("#" + identitylink).show();
 	});
+
+	$(".k1a5span1").click(function(){
+		var linkdetail = $(this).attr("linkdetail");
+		$(".k1a5span1").removeClass("k1a5span2");
+		$(this).addClass("k1a5span2");
+		$(".k1a5rightcontent").hide();
+		$("#" + linkdetail).show();
+	});
 });
